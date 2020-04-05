378bed698fa8eb2979b44446aed231d5a4f8101b
Sergio Burgueño <burguer@gmail.com>
2020-04-04 17:10:51 -0700

    add product list initial data

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
 
 @Component({
   selector: 'app-product-list',
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: any[] = [
+    {id: 1, name: 'Producto uno.'},
+    {id: 2, name: 'Producto dos.'},
+  ]
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
