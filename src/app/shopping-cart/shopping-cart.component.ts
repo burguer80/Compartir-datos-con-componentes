fab42354bcb6944df649faaa8403f0883c6add9c
Sergio Burgueño <burguer@gmail.com>
2020-04-04 20:39:11 -0700

    add input property

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -11,7 +11,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  productsBasket: string[] = ['💻', '🎮', '📱'];
+  @Input() productsBasket: string[] = ['💻', '🎮', '📱'];
 
   ngOnInit(): void {
   }
